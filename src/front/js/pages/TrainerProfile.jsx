895a6464a24c6b6bdd50be76685200300e6b6e18
Matteo Bertuzzi <146861948+matteobertuzzi@users.noreply.github.com>
2024-03-27 20:47:32 +0000

    fix trainerProfile

--- src/front/js/pages/TrainerProfile.jsx
+++ src/front/js/pages/TrainerProfile.jsx
@@ -10,31 +10,33 @@ const TrainerProfile = () => {
     let profilePictureMan = 'https://st3.depositphotos.com/9998432/13335/v/450/depositphotos_133351928-stock-illustration-default-placeholder-man-and-woman.jpg'
     let profilePictureWoman = 'https://png.pngtree.com/png-vector/20220607/ourmid/pngtree-person-gray-photo-placeholder-woman-in-t-shirt-on-white-background-png-image_4853921.png'
 
-    useEffect(async () => {
-        const id = store.currentUser.id
-        // const token = localStorage.getItem("accessToken");
-        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxMTU2MTE2OSwianRpIjoiNWE2YTgyNjktNGQyNy00ZjhkLWE2ZDYtYzg5ODI2ZTU1YTU2IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6eyJ0cmFpbmVyIjoiYS5iQGMuY29tIiwicm9sZSI6InRyYWluZXJzIiwiaWQiOjF9LCJuYmYiOjE3MTE1NjExNjksImNzcmYiOiI2Mzg1MmJjZC02NDU4LTRjODgtOTYzMS0yNmRhODFhODJjM2EiLCJleHAiOjE3MTE1NjIwNjl9.d4g8-iiedyw59c5dO-Sw9S37Z3nRAekoDQhqEpqtIrc'
-        if (!token) {
-            console.error("No access token provided!");
-            return null;
+    useEffect(() => {
+        async function fetchTrainer() {
+            const id = store.currentUser.id
+            // const token = localStorage.getItem("accessToken");
+            const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxMTU2MTE2OSwianRpIjoiNWE2YTgyNjktNGQyNy00ZjhkLWE2ZDYtYzg5ODI2ZTU1YTU2IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6eyJ0cmFpbmVyIjoiYS5iQGMuY29tIiwicm9sZSI6InRyYWluZXJzIiwiaWQiOjF9LCJuYmYiOjE3MTE1NjExNjksImNzcmYiOiI2Mzg1MmJjZC02NDU4LTRjODgtOTYzMS0yNmRhODFhODJjM2EiLCJleHAiOjE3MTE1NjIwNjl9.d4g8-iiedyw59c5dO-Sw9S37Z3nRAekoDQhqEpqtIrc'
+            if (!token) {
+                console.error("No access token provided!");
+                return null;
+            }
+            const options = {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            };
+            const url = process.env.BACKEND_URL + `/api/trainers/${id}`
+            const response = await fetch(url, options)
+            if (!response.ok) {
+                console.error(`Error fetching trainer data. HTTP Status ${response.status}`)
+                return null
+            }
+            const data = await response.json();
+            const trainerData = data.trainer;
+            let trainer = trainerData;
+            console.log(trainer);
+            return trainer
         }
-        const options = {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        };
-        const url = process.env.BACKEND_URL + `/api/trainers/${id}`
-        const response = await fetch(url, options)
-        if (!response.ok) {
-            console.error(`Error fetching trainer data. HTTP Status ${response.status}`)
-            return null
-        }
-        const data = await response.json();
-        const trainerData = data.trainer;
-        let trainer = trainerData;
-        console.log(trainer);
-        return trainer
     }, [])
 
     return (
