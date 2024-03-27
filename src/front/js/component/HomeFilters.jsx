9199fd1dbdeba10b774f5132c3ff26919a1891ec
Matteo Bertuzzi <146861948+matteobertuzzi@users.noreply.github.com>
2024-03-27 19:48:06 +0000

    filters

--- src/front/js/component/HomeFilters.jsx
+++ src/front/js/component/HomeFilters.jsx
@@ -1,15 +1,24 @@
-import React, { useState, useContext } from 'react';
-import { Context } from '../store/appContext';
+import React, { useState } from 'react';
 import { Form, DropdownButton, Dropdown, Button } from 'react-bootstrap';
 import InputGroup from 'react-bootstrap/InputGroup';
 
 
-const HomeFilters = () => {
-    const { store, actions } = useContext(Context);
+const HomeFilters = ({ filters, onFilterChange, onFilterSubmit }) => {
     const [trainingType, setTrainingtype] = useState('');
     const [trainingLevel, setTraininglevel] = useState('');
 
-    const userClasses = store.userClasses;
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        onFilterSubmit(filters);
+    };
+
+    const handleFilters = (event) => {
+        const { name, value } = event.target;
+        onFilterChange({
+            ...filters,
+            [name]: value,
+        });
+    };
 
 
     return (
@@ -24,27 +33,31 @@ const HomeFilters = () => {
                     <i className="fa-solid fa-magnifying-glass"></i>
                 </Button>
             </InputGroup>
-            <Form>
+            <Form onSubmit={handleFormSubmit}>
                 <Form.Group controlId="trainingType" className='my-4'>
                     <Form.Label>Training Type</Form.Label>
                     <DropdownButton
                         title={trainingType || 'Select training type'}
-                        onSelect={(eventKey) => setTrainingtype(eventKey)}
+                        name='trainingType'
+                        value={filters.trainingType}
+                        onChange={handleFilters}
                     >
-                        <Dropdown.Item eventKey="hiit">HIIT</Dropdown.Item>
-                        <Dropdown.Item eventKey="spinning">Spinning</Dropdown.Item>
-                        <Dropdown.Item eventKey="aerobics">Aerobics</Dropdown.Item>
+                        <Dropdown.Item value="hiit">HIIT</Dropdown.Item>
+                        <Dropdown.Item value="spinning">Spinning</Dropdown.Item>
+                        <Dropdown.Item value="aerobics">Aerobics</Dropdown.Item>
                     </DropdownButton>
                 </Form.Group>
-                <Form.Group controlId="priceRangeFilter" className='my-4'>
+                <Form.Group controlId="trainingLevel" className='my-4'>
                     <Form.Label>Training Level</Form.Label>
                     <DropdownButton
                         title={trainingLevel || 'Select training level'}
-                        onSelect={(eventKey) => setTraininglevel(eventKey)}
+                        name='trainingLevel'
+                        value={filters.trainingLevel}
+                        onChange={handleFilters}
                     >
-                        <Dropdown.Item eventKey="beginner">Beginner</Dropdown.Item>
-                        <Dropdown.Item eventKey="intermediate">Intermediate</Dropdown.Item>
-                        <Dropdown.Item eventKey="advanced">Advanced</Dropdown.Item>
+                        <Dropdown.Item value="beginner">Beginner</Dropdown.Item>
+                        <Dropdown.Item value="intermediate">Intermediate</Dropdown.Item>
+                        <Dropdown.Item value="advanced">Advanced</Dropdown.Item>
                     </DropdownButton>
                 </Form.Group>
                 <Button variant="primary" type="submit">
--- src/front/js/pages/Homepage.jsx
+++ src/front/js/pages/Homepage.jsx
@@ -4,14 +4,24 @@ import { Form, DropdownButton, Dropdown, Button } from 'react-bootstrap';
 import InputGroup from 'react-bootstrap/InputGroup';
 import HomeClassList from '../component/HomeClassList.jsx';
 import HomeUserClasses from '../component/HomeUserClasses.jsx';
+import HomeFilters from '../component/HomeFilters.jsx';
 
 
 const Homepage = () => {
     const { store, actions } = useContext(Context);
-    const [trainingType, setTrainingtype] = useState('');
-    const [trainingLevel, setTraininglevel] = useState('');
+    const [filters, setFilters] = useState({
+        trainingLevel: '',
+        trainingType: ''
+    })
 
-    const userClasses = store.userClasses;
+    const handleFilters = (newFilters) => {
+        setFilters(newFilters);
+    };
+
+    const handleFilterSubmit = (event) => {
+        event.preventDefault(); 
+        console.log('Filters submitted:', filters);
+    };
 
 
     return (
@@ -19,46 +29,10 @@ const Homepage = () => {
             <div className='container-fluid'>
                 <div className='row'>
                     <div className='col-lg-3 col-md-3 col-sm-10 d-flex-column justify-content-center' style={{ 'backgroundColor': '#D3D3D3', 'height': '100vh' }}>
-                        <InputGroup className="my-5 px-3">
-                            <Form.Control
-                                placeholder="Search city"
-                                aria-label="Search city"
-                                aria-describedby="search-city"
-                            />
-                            <Button variant="outline-secondary" id="search-city">
-                                <i className="fa-solid fa-magnifying-glass"></i>
-                            </Button>
-                        </InputGroup>
-                        <Form>
-                            <Form.Group controlId="trainingType" className='my-4'>
-                                <Form.Label>Training Type</Form.Label>
-                                <DropdownButton
-                                    title={trainingType || 'Select training type'}
-                                    onSelect={(eventKey) => setTrainingtype(eventKey)}
-                                >
-                                    <Dropdown.Item eventKey="hiit">HIIT</Dropdown.Item>
-                                    <Dropdown.Item eventKey="spinning">Spinning</Dropdown.Item>
-                                    <Dropdown.Item eventKey="aerobics">Aerobics</Dropdown.Item>
-                                </DropdownButton>
-                            </Form.Group>
-                            <Form.Group controlId="priceRangeFilter" className='my-4'>
-                                <Form.Label>Training Level</Form.Label>
-                                <DropdownButton
-                                    title={trainingLevel || 'Select training level'}
-                                    onSelect={(eventKey) => setTraininglevel(eventKey)}
-                                >
-                                    <Dropdown.Item eventKey="beginner">Beginner</Dropdown.Item>
-                                    <Dropdown.Item eventKey="intermediate">Intermediate</Dropdown.Item>
-                                    <Dropdown.Item eventKey="advanced">Advanced</Dropdown.Item>
-                                </DropdownButton>
-                            </Form.Group>
-                            <Button variant="primary" type="submit">
-                                Search classes
-                            </Button>
-                        </Form>
+                        <HomeFilters filters={filters} onFilterChange={handleFilters} onFilterSubmit={handleFilterSubmit}/>
                     </div>
                     <div className='col-lg-6 col-md-6 col-sm-10'>
-                        <HomeClassList />
+                        <HomeClassList filters={filters} />
                     </div>
                     <div className='col-lg-3 col-md-3 col-sm-10' style={{ 'backgroundColor': '#D3D3D3', 'height': '100vh' }}>
                         <HomeUserClasses />
