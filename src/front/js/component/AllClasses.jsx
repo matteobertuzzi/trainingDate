273dee421e61ca91dfa336f1eb9604636859e45d
Matteo Bertuzzi <146861948+matteobertuzzi@users.noreply.github.com>
2024-04-13 18:56:31 +0000

    add AllClasses

--- src/front/js/component/AllClasses.jsx
+++ src/front/js/component/AllClasses.jsx
@@ -1,48 +1,129 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { Context } from '../store/appContext';
-import { Button,  Card, Alert} from 'react-bootstrap/';
-import MapModal from './MapModal.jsx';
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../store/appContext";
+import { Container, Row, Col, Card, Button, Alert, Nav } from 'react-bootstrap';
+import Loading from '../component/Loading.jsx';
+import { useParams, Link } from 'react-router-dom';
+import { RiArrowGoBackLine } from "react-icons/ri";
 import { IoIosWarning } from "react-icons/io";
 
-
-const AllClasses = ({ filters }) => {
+const AllClasses = () => {
     const { store, actions } = useContext(Context);
-    const allClasses = store.allClasses;
-    const currentUser = JSON.parse(localStorage.getItem('availableAccount'));
-    const isUser = currentUser && currentUser.role === 'users';
+    const { allClasses } = store;
+    const [currentDateTime, setCurrentDateTime] = useState(new Date());
+    const [pastClasses, setPastClasses] = useState([]);
+    const [futureClasses, setFutureClasses] = useState([]);
+    const [activeTab, setActiveTab] = useState("past");
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentDateTime(new Date());
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
+        // Filter past and future classes
+        const past = [];
+        const future = [];
+        allClasses.forEach(classItem => {
+            const classStartTime = new Date(classItem.start_date);
+            if (classStartTime <= currentDateTime) {
+                past.push(classItem);
+            } else {
+                future.push(classItem);
+            }
+        });
+        setPastClasses(past);
+        setFutureClasses(future);
+    }, [allClasses, currentDateTime]);
 
     return (
-        <>
-            <div className="container mt-4">
-                <h2 className='text-center my-5'>Clases disponibles</h2>
-                <div className="row">
-                    {allClasses.length !== 0 ? (
-                        allClasses.map((oneClass) => (
-                            <div className="col-md-4 mb-4" key={oneClass.id}>
-                                <Card>
-                                    <Card.Img variant="top" src="https://www.shape.com/thmb/vMUCGBBuieN6Y5h0bgCqzt0Vf7o=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/fb-interval-training-workouts-c93316d5efe14dee93c6d33ccdb6cd31.jpg" />
-                                    <Card.Body>
-                                        <Card.Title>{oneClass.class_name ? oneClass.class_name : 'Clase de entrenamiento'}</Card.Title>
-                                        <Card.Text>{oneClass.class_details ? oneClass.class_details : 'Clase de entrenamiento'}</Card.Text>
-                                        <div className='d-flex justify-content-center'>
-                                            <MapModal className="mx-3" addressData={[oneClass.city, oneClass.postal_code, oneClass.street_name, oneClass.street_number]} />
-                                        </div>
+        <Container className="min-vh-100 my-4">
+            <Row className='m-3 d-flex flex-row gap-2 justify-content-between align-items-center'>
+                <Col>
+                    <Link to={"/"}>
+                        <RiArrowGoBackLine /> Volver atrás
+                    </Link>
+                </Col>
+            </Row>
+            <h1 className="text-center mb-4">Todas las Clases</h1>
+            <Nav className="d-flex flex-row justify-content-center align-items-center" variant="tabs" activeKey={activeTab} onSelect={(selectedKey) => setActiveTab(selectedKey)}>
+                <Nav.Item>
+                    <Nav.Link eventKey="past">Pasadas</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="future">Futuras</Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <Row xs={1} md={2} lg={3} className="justify-content-center">
+                {(activeTab === "past" && pastClasses.length !== 0) && (
+                    <div className="d-flex flex-row flex-wrap align-items-center justify-content-center">
+                        {pastClasses.map(classItem => (
+                            <div key={classItem.id} className="d-flex gap-2 m-2 justify-content-center align-items-center">
+                                <Card border="primary" style={{ width: '18rem' }}>
+                                    <Card.Header>{classItem.id}</Card.Header>
+                                    <Card.Body className="d-flex justify-content-between align-items-center">
+                                        <section>
+                                            <Card.Text>Ciudad: {classItem.city}</Card.Text>
+                                            <Card.Text>Codigo Postal:{classItem.postal_code}</Card.Text>
+                                            <Card.Text>Calle: {classItem.street_name}</Card.Text>
+                                            <Card.Text>Precio: {classItem.price / 100}<span>€</span></Card.Text>
+                                        </section>
+                                        <section className="d-flex flex-column gap-2">
+                                            <Button variant="danger" onClick={() => handleClick(classItem.trainer, classItem.id)}>Delete</Button>
+                                        </section>
                                     </Card.Body>
                                 </Card>
                             </div>
-                        ))
-                    ) : (
-                        <Alert variant="warning" className="d-flex flex-column justify-content-center align-items-center">
-                            <Alert.Heading className="d-flex flex-row align-items-center justify-content-center gap-2"><IoIosWarning />No hay clases disponibles</Alert.Heading>
+                        ))}
+                    </div>
+                )}
+            </Row>
+            <Row>
+                {(activeTab === "future" && futureClasses.length !== 0) && (
+                    <div className="d-flex flex-row flex-wrap align-items-center justify-content-center">
+                        {futureClasses.map(classItem => (
+                            <div key={classItem.id} className="d-flex gap-2 m-3 justify-content-center align-items-center">
+                                <Card border="primary" style={{ width: '18rem' }}>
+                                    <Card.Header>{classItem.id}</Card.Header>
+                                    <Card.Body className="d-flex justify-content-between align-items-center">
+                                        <section>
+                                            <Card.Text>Ciudad: {classItem.city}</Card.Text>
+                                            <Card.Text>Codigo Postal:{classItem.postal_code}</Card.Text>
+                                            <Card.Text>Calle: {classItem.street_name}</Card.Text>
+                                            <Card.Text>Precio: {classItem.price / 100}<span>€</span></Card.Text>
+                                        </section>
+                                        <section className="d-flex flex-column gap-2">
+                                            <Button variant="danger" onClick={() => handleClick(classItem.trainer, classItem.id)}>Delete</Button>
+                                        </section>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </Row>
+            <Row className="d-flex justify-content-center align-items-center">
+                {(activeTab === "past" && pastClasses.length === 0) && (
+                    <Col className="d-flex justify-content-center align-items-center m-3">
+                        <Alert variant="warning" className="d-flex flex-column justify-content-center align-items-center w-75">
+                            <Alert.Heading className="d-flex flex-row align-items-center justify-content-center gap-2"><IoIosWarning />No hay clases pasadas disponibles</Alert.Heading>
+                        </Alert>
+                    </Col>
+                )}
+                {(activeTab === "future" && futureClasses.length === 0) && (
+                    <Col className="d-flex justify-content-center align-items-center m-4">
+                        <Alert variant="warning" className="d-flex flex-column justify-content-center align-items-center w-75">
+                            <Alert.Heading className="d-flex flex-row align-items-center justify-content-center gap-2"><IoIosWarning />No hay clases futuras disponibles</Alert.Heading>
                             <p>
-                                De momento no hay clases disponibles, intentalo más tarde.
+                                Parece que aún no tienes ninguna clase futura.
                             </p>
                         </Alert>
-                    )}
-                </div>
-            </div>
-        </>
-    );
+                    </Col>
+                )}
+            </Row>
+        </Container >
+    )
 };
 
 export default AllClasses;
