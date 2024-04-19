6ccf1804943042000d7822b3fe8a9262d5208b4f
Matteo Bertuzzi <146861948+matteobertuzzi@users.noreply.github.com>
2024-04-19 14:42:31 +0000

    fix homepage style

--- src/front/js/component/ClassesCarousel.jsx
+++ src/front/js/component/ClassesCarousel.jsx
@@ -1,64 +1,52 @@
 import React, { useContext } from 'react';
 import { Context } from '../store/appContext';
-import Carousel from 'react-bootstrap/Carousel';
-import { BsChevronCompactRight, BsChevronCompactLeft } from "react-icons/bs";
-import Card from 'react-bootstrap/Card';
-import { Button } from 'react-bootstrap';
-import Loading from './Loading.jsx';
-import ClassModal from './ClassModal.jsx';
+import SwiperCore from 'swiper/core';
+import { Navigation, Pagination, EffectCoverflow } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Button, Card } from 'react-bootstrap';
+import 'swiper/swiper-bundle.css';
 import MapModal from './MapModal.jsx';
 
-const placeholderImageUrl = 'https://www.tiege.com/cdn/shop/articles/couple-working-out-at-gym.jpg';
+// Install Swiper modules
+SwiperCore.use([Navigation, Pagination, EffectCoverflow]);
 
 const ClassesCarousel = () => {
     const { store, actions } = useContext(Context);
     const { allClasses } = store;
 
-    // Helper function to chunk the array into groups of three
-    const chunkArray = (arr, chunkSize) => {
-        const chunkedArr = [];
-        for (let i = 0; i < arr.length; i += chunkSize) {
-            chunkedArr.push(arr.slice(i, i + chunkSize));
-        }
-        return chunkedArr;
-    };
-
-    // Splitting allClasses into groups of three
-    const classGroups = chunkArray(allClasses, 3);
-
     return (
-        <>
-            {allClasses.length !== 0 ?
-                (<Carousel prevIcon={<BsChevronCompactLeft />} nextIcon={<BsChevronCompactRight />}>
-                    {classGroups.map((group, index) => (
-                        <Carousel.Item key={index}>
-                            <div className="row d-flex flex-row align-items-center justify-content-center">
-                                {group.map((oneClass, innerIndex) => (
-                                    <div className='col-xl-3 col-lg-6 col-sm-7 col-sm-10 d-flex flex-row justify-content-center align-items-center h-100' key={innerIndex}>
-                                        <Card className='my-3'>
-                                            <Card.Img variant="top" src={placeholderImageUrl} />
-                                            <Card.Body>
-                                                <Card.Title>{oneClass.class_name ? oneClass.class_name : 'Clase de entrenamiento'}</Card.Title>
-                                                <Card.Text>
-                                                    {oneClass.class_details ? oneClass.class_details : 'Clase de entrenamiento'}
-                                                </Card.Text>
-                                                <div className="d-flex justify-content-around">
-                                                    <ClassModal userClass={oneClass} className='mx-3' />
-                                                    <MapModal addressData={[oneClass.city, oneClass.postal_code, oneClass.street_name, oneClass.street_number]} className='mx-3' />
-                                                </div>
-                                            </Card.Body>
-                                        </Card>
-                                    </div>
-                                ))}
-                            </div>
-                        </Carousel.Item>
-                    ))}
-                </Carousel>)
-                :
-                <Loading />
-            }
-        </>
-    )
-}
+        <div className="carousel-container">
+            <Swiper
+                effect={'coverflow'}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={'auto'}
+                coverflowEffect={{
+                    rotate: 50,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 1,
+                    slideShadows: true,
+                }}
+                pagination={true}
+                modules={[EffectCoverflow, Pagination]}
+                className="swiper"
+                style={{ width: '100%', paddingTop: '30px', paddingBottom: '30px' }}
+            >
+                {allClasses.map((oneClass) => (
+                    <SwiperSlide className="swiper-slide d-flex flex-column align-items-center" style={{ backgroundPosition: 'center', backgroundSize: 'cover', width: '300px', height: '300px' }}>
+                        <h2>{oneClass.class_name}</h2>
+                        <img className='w-100 d-block' src='https://www.regymenfitness.com/wp-content/uploads/2021/12/Sportive-serious-people-liftin-1080x675.jpg' />
+                        <div className='my-3'>
+                            <MapModal addressData={[oneClass.city, oneClass.postal_code, oneClass.street_name, oneClass.street_number]} />
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    );
+};
 
 export default ClassesCarousel;
+
+
--- src/front/js/component/UnloggedView.jsx
+++ src/front/js/component/UnloggedView.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Accordion, Container, Card, Row, Col } from 'react-bootstrap';
+import { Button, Accordion, Container, Card, Row, Col, Carousel } from 'react-bootstrap';
 import { FaDumbbell } from 'react-icons/fa';
 import { LogInModal } from './LogInModal.jsx';
 import ClassesCarousel from './ClassesCarousel.jsx';
@@ -13,116 +13,89 @@ const UnloggedView = () => {
 
     return (
         <Container fluid className="p-0">
-            <Card className="text-white" style={{ borderWidth: '0', height: '40vh' }}>
-                <Card.Img
-                    className="img-fluid rounded-0"
-                    src={img1}
-                    alt="Training Date"
-                    style={{ opacity: '0.8', objectFit: 'cover', height: '100%' }}
-                />
-                <Card.ImgOverlay className="d-flex flex-column gap-2 justify-content-center align-items-center">
-                    <div style={{ opacity: '0.8', backgroundColor: 'white', borderRadius: '10%', color: 'black', padding: '10px' }} className="d-flex justify-content-between">
-                        <div className='d-flex justify-content-center align-items-center' style={{ width: '30%' }}>
-                            <FaDumbbell size={100} />
-                        </div>
-                        <div className='d-flex flex-column my-2 justify-content-center align-items-center' style={{ width: '70%' }}>
-                            <h2 style={{ fontWeight: 'bold' }}>¡Bienvenido a Training Date!</h2>
-                            <h5 className='text-center'>"¿Listo para conectar con entrenadores y compañeros fitness de toda España?"</h5>
+            <Row>
+                <Card className="text-white" style={{ borderWidth: '0', height: '40vh' }}>
+                    <Card.Img
+                        className="img-fluid rounded-0"
+                        src={img1}
+                        alt="Training Date"
+                        style={{ opacity: '0.8', objectFit: 'cover', height: '100%' }}
+                    />
+                    <Card.ImgOverlay className="d-flex justify-content-start align-items-center">
+                        <div className="d-flex flex-column justify-content-center align-items-center" style={{ opacity: '0.9', backgroundColor: 'white', color: 'black', padding: '10px', width: '30%', marginLeft: '5%' }}>
+                            <h2 className='mt-3' style={{ fontWeight: 'bold', fontSize: '36px', color: '#333333', textTransform: 'uppercase', letterSpacing: '2px', textAlign: 'center', textShadow: '0px 2px 2px rgba(0, 0, 0, 0.1)', lineHeight: '1.5' }}>¡Bienvenido a Training Date!</h2>
+                            <h5 className='text-center mx-3 my-2'>"¿Listo para conectar con entrenadores y compañeros fitness de toda España?"</h5>
                             <Link to='/signup'>
-                                <Button variant='primary' className='my-3' style={{ height: '50px', width: '200px', fontWeight: 'bold' }}>Regístrate Ahora</Button>
+                                <Button variant='primary' className='my-3' style={{ height: '50px', width: '200px', fontWeight: 'bold', fontSize: '20px', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)', border: 'none', backgroundImage: 'linear-gradient(to right, #ff8a00, #da1b60)', color: '#ffffff', textShadow: '0px 2px 2px rgba(0, 0, 0, 0.2)', transition: 'transform 0.3s ease' }}
+                                    onMouseEnter={(e) => e.target.style.transform = 'scale(1.05)'}
+                                    onMouseLeave={(e) => e.target.style.transform = 'scale(1)'}
+                                >
+                                    Regístrate Ahora
+                                </Button>
                             </Link>
                         </div>
-                    </div>
-                </Card.ImgOverlay>
-            </Card >
-            <Row className="bg-light py-5">
-                <Col md={12} className="text-center my-2">
-                    <h2>Descubre el Mundo del Fitness con Nosotros</h2>
-                    <p className="lead px-3">¿Listo para dar el siguiente paso en tu viaje fitness? Encuentra la motivación y el apoyo que necesitas para alcanzar tus objetivos. Conéctate con entrenadores y compañeros de fitness de toda España y descubre nuevas oportunidades para crecer y mejorar.</p>
-                </Col>
+                    </Card.ImgOverlay>
+                </Card >
             </Row>
-            <Row className="text-center mt-4">
-                <h2 style={{ fontWeight: 'bold', marginBottom: '30px', marginTop: '30px' }}>Para el usuario</h2>
-                <Col md={8} className='d-flex flex-column justify-content-center align-content-center'>
-                    <div className='mx-5 my-2'>
-                        <h4 style={{ fontWeight: 'bold' }}>Paso 1: Iniciar sesión y crear perfil</h4>
-                        <p>El primer paso es iniciar sesión en la aplicación y crear tu perfil de usuario. Puedes proporcionar detalles como tu nombre, dirección de correo electrónico, y otros datos relevantes.</p>
-                    </div>
-                    <div className='mx-5 my-2'>
-                        <h4 style={{ fontWeight: 'bold' }}>Paso 2: Explorar clases disponibles</h4>
-                        <p>Una vez que hayas creado tu perfil, puedes explorar todas las clases disponibles en la aplicación. Desde yoga hasta entrenamiento de fuerza, hay una variedad de opciones para elegir.</p>
-                    </div>
-                    <div className='mx-5 my-2'>
-                        <h4 style={{ fontWeight: 'bold' }}>Paso 3: Actualizar información y reservar clases</h4>
-                        <p>Puedes actualizar tu información de perfil en cualquier momento y reservar clases que te interesen. Además, puedes ver las clases que has reservado y administrar tus reservas desde tu perfil.</p>
-                    </div>
-                </Col>
-                <Col md={4} lg={4} className='d-flex justify-content-center align-items-center'>
-                    <Card className="h-100" style={{ width: '90%', borderRadius: '10px' }}>
-                        <Card.Img className="img-fluid" variant="top" src={img3} />
-                        <Card.Body className="d-flex flex-column">
-                            <Card.Title>Usuario</Card.Title>
-                            <Card.Text>
-                                ¿Buscas clases personalizadas? Regístrate como usuario y encuentra entrenadores y clases que se adapten a tus necesidades.
-                            </Card.Text>
-                            <div className='d-flex justify-content-center mt-auto'>
-                                <Link to='/signupUser'>
-                                    <Button variant="primary">Regístrate</Button>
-                                </Link>
-                            </div>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-            <Row className="text-center bg-light mt-4">
-                <h2 style={{ fontWeight: 'bold', marginBottom: '30px', marginTop: '30px' }}>Para el entrenador</h2>
-                <Col md={6} lg={4} className='d-flex justify-content-center align-items-center my-3'>
-                    <Card className="h-100" style={{ width: '90%', borderRadius: '10px' }}>
-                        <Card.Img variant="top" src={img2} />
-                        <Card.Body className="d-flex flex-column">
-                            <Card.Title>Entrenador</Card.Title>
-                            <Card.Text>
-                                ¿Eres entrenador? Únete a nuestra plataforma para compartir tus clases y entrenamientos con usuarios de toda España.
-                            </Card.Text>
-                            <div className='d-flex justify-content-center mt-auto'>
-                                <Link to='/signupTrainer'>
-                                    <Button variant="primary">Regístrate</Button>
-                                </Link>
-                            </div>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={8} className='d-flex flex-column justify-content-center align-content-center'>
-                    <div className='mx-5 my-2'>
-                        <h4 style={{ fontWeight: 'bold' }}>Paso 1: Envía tu certificado o título de estudio</h4>
-                        <p>EEl primer paso para convertirte en entrenador en nuestra plataforma es enviarnos tu certificado o título de estudio que demuestre tus habilidades y especializaciones relevantes. Este documento será revisado por nuestro equipo administrativo para verificar su autenticidad.</p>
-                    </div>
-                    <div className='mx-5 my-2'>
-                        <h4 style={{ fontWeight: 'bold' }}>Paso 2: Revisión y aprobación</h4>
-                        <p>Una vez que hayas enviado tu certificado, nuestro equipo administrativo lo revisará para asegurarse de que cumpla con nuestros requisitos. Si tu certificado es aprobado, recibirás una notificación por correo electrónico informándote sobre la aprobación.</p>
-                    </div>
-                    <div className='mx-5 my-2'>
-                        <h4 style={{ fontWeight: 'bold' }}>Paso 3: Inicia sesión y comienza a crear clases</h4>
-                        <p>Después de recibir la confirmación de que tu certificado ha sido aprobado, puedes iniciar sesión en nuestra plataforma como entrenador. Desde tu panel de control, tendrás la capacidad de crear clases y programas de entrenamiento personalizados para que los usuarios se inscriban.</p>
-                    </div>
-                    <div className='mx-5 my-2'>
-                        <h4 style={{ fontWeight: 'bold' }}>Paso 4: Interactúa con tus clientes</h4>
-                        <p>Una vez que hayas creado tus clases, podrás interactuar con tus clientes y gestionar sus inscripciones. Puedes comunicarte con ellos a través de mensajes directos, programar sesiones individuales o grupales y seguir su progreso.</p>
-                    </div>
-                </Col>
-            </Row>
-            <Row className="d-flex py-5 justify-content-center align-items-center">
-                <Card md={6} className="p-3 d-flex justify-content-center align-items-center" style={{ width: '80%' }}>
-                    <h4 className='text-center'>¿Ya estás registrado?</h4>
-                    <p className='text-center'>¡Bienvenido! Si ya estás registrado como entrenador o usuario, haz clic en el siguiente botón para iniciar tu sesión.</p>
-                    <div className='d-flex justify-content-center'>
-                        <Button onClick={() => setLoginModalShow(true)} style={{ marginTop: '10px', marginBottom: '10px' }}>Log in</Button>
-                    </div>
-                    <LogInModal show={loginModalShow} onHide={() => setLoginModalShow(false)} />
-                </Card>
+            <Row className='bg-light'>
+                <h2 className='text-center mt-3' style={{ fontWeight: 'bold', fontSize: '36px', color: '#333333', textTransform: 'uppercase', letterSpacing: '2px', textAlign: 'center', textShadow: '0px 2px 2px rgba(0, 0, 0, 0.1)' }}>Descubre nuestras Clases</h2>
+                <ClassesCarousel />
             </Row>
             <Row>
-                <ClassesCarousel />
+                <Carousel>
+                    <Carousel.Item>
+                        <Card style={{ width: '70vw', margin: '20px auto', boxShadow: '0 4px 8px rgba(0,0,0,0.1)' }}>
+                            <div className="row no-gutters">
+                                <div className="col-md-6">
+                                    <Card.Img variant="top" src={img3} style={{ height: '100%' }} />
+                                </div>
+                                <div className="col-md-6 d-flex align-items-center justify-content-center">
+                                    <Card.Body>
+                                        <Card.Title style={{ fontSize: '28px', fontWeight: 'bold', color: '#333', textAlign: 'center' }}>Encuentra tu clase ideal</Card.Title>
+                                        <Card.Text style={{ fontSize: '18px', textAlign: 'center', marginBottom: '20px' }}>
+                                            ¿Buscas clases personalizadas? Regístrate como usuario y encuentra entrenadores y clases que se adapten a tus necesidades.
+                                        </Card.Text>
+                                        <Link to='/signupUser' style={{ textAlign: 'center', display: 'block' }}>
+                                            <Button variant='primary' className='my-3' style={{ height: '50px', width: '200px', fontWeight: 'bold', fontSize: '20px', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)', border: 'none', backgroundImage: 'linear-gradient(to right, #ff8a00, #da1b60)', color: '#ffffff', textShadow: '0px 2px 2px rgba(0, 0, 0, 0.2)', transition: 'transform 0.3s ease' }}
+                                                onMouseEnter={(e) => e.target.style.transform = 'scale(1.05)'}
+                                                onMouseLeave={(e) => e.target.style.transform = 'scale(1)'}
+                                            >
+                                                Apúntate
+                                            </Button>
+                                        </Link>
+                                    </Card.Body>
+                                </div>
+                            </div>
+                        </Card>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <Card className="h-100" style={{ width: '70vw', borderRadius: '10px', margin: '20px auto', boxShadow: '0 4px 8px rgba(0,0,0,0.1)' }}>
+                            <div className="row no-gutters">
+                                <div className="col-md-6 d-flex align-items-center justify-content-center">
+                                    <Card.Body className="text-center">
+                                        <Card.Title style={{ fontSize: '28px', fontWeight: 'bold', color: '#333', textAlign: 'center' }}>¡Únete como Entrenador!</Card.Title>
+                                        <Card.Text style={{ fontSize: '18px', textAlign: 'center', marginBottom: '20px' }}>
+                                            ¿Eres entrenador? Únete a nuestra plataforma para compartir tus clases y entrenamientos con usuarios de toda España.
+                                        </Card.Text>
+                                        <div className='d-flex justify-content-center'>
+                                            <Link to='/signupTrainer'>
+                                                <Button variant='primary' className='my-3' style={{ height: '50px', width: '200px', fontWeight: 'bold', fontSize: '20px',  boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)', border: 'none', backgroundImage: 'linear-gradient(to right, #ff8a00, #da1b60)', color: '#ffffff', textShadow: '0px 2px 2px rgba(0, 0, 0, 0.2)', transition: 'transform 0.3s ease' }}
+                                                    onMouseEnter={(e) => e.target.style.transform = 'scale(1.05)'}
+                                                    onMouseLeave={(e) => e.target.style.transform = 'scale(1)'}
+                                                >
+                                                    Apúntate
+                                                </Button>
+                                            </Link>
+                                        </div>
+                                    </Card.Body>
+                                </div>
+                                <div className="col-md-6">
+                                    <Card.Img variant="top" src={img2} />
+                                </div>
+                            </div>
+                        </Card>
+                    </Carousel.Item>
+                </Carousel>
             </Row>
             <Row className='d-flex flex-column justify-content-center align-items-center p-3'>
                 <Col className='pb-4'>
